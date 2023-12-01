197714e8456f30dc1c36d90c0f0c8024b273a6ec
猿小天 <1638245306@qq.com>
2023-12-01 17:31:37 +0800

    修复BUG:
    1.修复settings中上传文件的bug;
    2.优化字典展示

--- web/src/settings.ts
+++ web/src/settings.ts
@@ -3,6 +3,7 @@ import { FastCrud, useTypes } from '@fast-crud/fast-crud';
 const { getType } = useTypes();
 import '@fast-crud/fast-crud/dist/style.css';
 import { setLogger } from '@fast-crud/fast-crud';
+import { getBaseURL } from '/@/utils/baseUrl';
 // element
 import ui from '@fast-crud/ui-element';
 import { request } from '/@/utils/service';
