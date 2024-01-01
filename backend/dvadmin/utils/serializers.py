ac7e8a776425a8f93ddc9e02a28c62008c0e4fb2
李强 <1206709430@qq.com>
2024-01-01 22:31:43 +0800

    feat: CustomModelSerializer序列化器中dept_belong_id 添加默认值

--- backend/dvadmin/utils/serializers.py
+++ backend/dvadmin/utils/serializers.py
@@ -26,7 +26,7 @@ class CustomModelSerializer(DynamicFieldsMixin, ModelSerializer):
     # 修改人的审计字段名称, 默认modifier, 继承使用时可自定义覆盖
     modifier_field_id = "modifier"
     modifier_name = serializers.SerializerMethodField(read_only=True)
-    dept_belong_id = serializers.IntegerField(required=False)
+    dept_belong_id = serializers.IntegerField(required=False, default=None)
 
     def get_modifier_name(self, instance):
         if not hasattr(instance, "modifier"):
