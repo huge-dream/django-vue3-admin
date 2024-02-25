26bbf67da8c0b9dab6ce4329ebcdc61e7f51194a
猿小天 <1638245306@qq.com>
2024-02-25 15:03:07 +0800

    新功能: 角色授权字段权限判断

--- web/src/views/system/role/components/PermissionComNew/index.vue
+++ web/src/views/system/role/components/PermissionComNew/index.vue
@@ -49,7 +49,7 @@
               </el-checkbox>
             </div>
 
-            <div class="pccm-item">
+            <div class="pccm-item" v-if="item.columns&&item.columns.length>0">
               <p>对这些数据有以下字段权限</p>
 
               <ul class="columns-list">
