988daf03dd31dbe85d57299442ceb9c80d2a6290
李强 <1206709430@qq.com>
2024-03-18 14:28:25 +0800

    优化系统配置表的key值大小

--- backend/dvadmin/system/models.py
+++ backend/dvadmin/system/models.py
@@ -485,7 +485,7 @@ class SystemConfig(CoreModel):
         help_text="父级",
     )
     title = models.CharField(max_length=50, verbose_name="标题", help_text="标题")
-    key = models.CharField(max_length=20, verbose_name="键", help_text="键", db_index=True)
+    key = models.CharField(max_length=100, verbose_name="键", help_text="键", db_index=True)
     value = models.JSONField(max_length=100, verbose_name="值", help_text="值", null=True, blank=True)
     sort = models.IntegerField(default=0, verbose_name="排序", help_text="排序", blank=True)
     status = models.BooleanField(default=True, verbose_name="启用状态", help_text="启用状态")
