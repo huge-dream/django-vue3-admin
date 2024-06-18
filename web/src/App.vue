1f81ac5854dd2ded3cc0504440f54983af2b2193
李强 <1206709430@qq.com>
2024-06-18 12:26:34 +0800

    feat: 取消升级提示

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,7 +5,7 @@
 		<LockScreen v-if="themeConfig.isLockScreen" />
 		<Setings ref="setingsRef" v-show="themeConfig.lockScreenTime > 1" />
 		<CloseFull v-if="!themeConfig.isLockScreen" />
-		<Upgrade v-if="getVersion" />
+<!--		<Upgrade v-if="getVersion" />-->
 	</el-config-provider>
 </template>
 
