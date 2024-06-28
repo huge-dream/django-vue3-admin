c2b0c3f25baa8bf8d5ff7cd2e35350096e179f60
李小涛 <1537080775@qq.com>
2024-06-28 23:35:05 +0800

    feat(role_menu_20240628): 优化权限配置，增强用户体验
    - 修复无法保存菜单授权bug
    - 列权限增加禁用状态的逻辑判断

--- web/src/views/system/role/components/PermissionComNew/types.ts
+++ web/src/views/system/role/components/PermissionComNew/types.ts
@@ -1,36 +1,29 @@
 export interface DataPermissionRangeType {
-  label: string;
-  value: number;
+	label: string;
+	value: number;
 }
 
 export interface CustomDataPermissionDeptType {
-  id: number;
-  name: string;
-  patent: number;
-  children: CustomDataPermissionDeptType[]
+	id: number;
+	name: string;
+	patent: number;
+	children: CustomDataPermissionDeptType[];
 }
 
 export interface CustomDataPermissionMenuType {
-  id: number;
-  name: string;
-  is_catalog: boolean;
-  menuPermission: { id: number; name: string; value: string }[] | null;
-  columns: { id: number; name: string; title: string }[] | null;
-  children: CustomDataPermissionMenuType[]
-}
-
-export interface MenusType{
-  id: string;
-  name: string;
-  isCheck: boolean;
-  radio: string;
-  btns: { id:number,name: string; value: string; isCheck: boolean; data_range: number; dept:object; name:string }[];
-  columns: { [key: string]: boolean | string; }[]
+	id: number;
+	name: string;
+	is_catalog: boolean;
+	menuPermission: { id: number; name: string; value: string }[] | null;
+	columns: { id: number; name: string; title: string }[] | null;
+	children: CustomDataPermissionMenuType[];
 }
 
 export interface MenuDataType {
-  id: string;
-  name: string;
-  menus:MenusType[];
+	id: string;
+	name: string;
+	isCheck: boolean;
+	btns: { id: number; name: string; value: string; isCheck: boolean; data_range: number; dept: object }[];
+	columns: { [key: string]: boolean | string; }[];
+	children: MenuDataType[];
 }
-
