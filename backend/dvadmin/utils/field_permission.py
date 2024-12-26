dddafa4826ed9ff9730096e86e8cf38b40da68c7
李小涛 <1537080775@qq.com>
2024-12-26 08:56:19 +0800

    feat(20241226-fieldpermission): 优化列权限逻辑
    1、后端优化，当多个角色的时候，合并列权限配置
    2、前端优化，有多级表头时，列权限设置无效的bug

--- backend/dvadmin/utils/field_permission.py
+++ backend/dvadmin/utils/field_permission.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from itertools import groupby
-
 from django.db.models import F
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -12,7 +10,7 @@ from dvadmin.utils.models import get_custom_app_models
 
 
 class FieldPermissionMixin:
-    @action(methods=['get'], detail=False,permission_classes=[IsAuthenticated])
+    @action(methods=['get'], detail=False, permission_classes=[IsAuthenticated])
     def field_permission(self, request):
         """
         获取字段权限
@@ -27,45 +25,32 @@ class FieldPermissionMixin:
         if finded is False:
             return []
         user = request.user
-        if user.is_superuser==1:
-            data = MenuField.objects.filter( model=model['model']).values('field_name')
+        # 创建一个默认字典来存储最终的结果
+        result = {}
+        if user.is_superuser == 1:
+            data = MenuField.objects.filter(model=model['model']).values('field_name')
             for item in data:
-                item['is_create'] = True
-                item['is_query'] = True
-                item['is_update'] = True
-        else:
-            roles = request.user.role.values_list('id', flat=True)
-            data= FieldPermission.objects.filter(
-                 field__model=model['model'],role__in=roles
-            ).values( 'is_create', 'is_query', 'is_update',field_name=F('field__field_name'))
-            
+                field_name = item.pop('field_name')
+                result[field_name] = {}
+                result[field_name]['is_create'] = True
+                result[field_name]['is_query'] = True
+                result[field_name]['is_update'] = True
+            else:
+                roles = request.user.role.values_list('id', flat=True)
+                data = FieldPermission.objects.filter(
+                    field__model=model['model'], role__in=roles
+                ).values('is_create', 'is_query', 'is_update', field_name=F('field__field_name'))
             """
             合并权限
-
-            这段代码首先根据 field_name 对列表进行排序，
-            然后使用 groupby 按 field_name 进行分组。
-            对于每个组，它创建一个新的字典 merged，
-            并遍历组中的每个字典，将布尔值字段使用逻辑或（or）操作符进行合并（如果 merged 中还没有该字段，则默认为 False），
-            其他字段（如 field_name）则直接取组的关键字（即 key）
             """
-
-            # 使用field_name对列表进行分组, # groupby 需要先对列表进行排序，因为它只能对连续相同的元素进行分组。
-            grouped = groupby(sorted(list(data), key=lambda x: x['field_name']), key=lambda x: x['field_name'])
-
-            data = []
-
-            # 遍历分组，合并权限
-            for key, group in grouped:
-
-                # 初始化一个空字典来存储合并后的结果
-                merged = {}
-                for item in group:
-                    # 合并权限， True值优先
-                    merged['is_create'] = merged.get('is_create', False) or item['is_create']
-                    merged['is_query'] = merged.get('is_query', False) or item['is_query']
-                    merged['is_update'] = merged.get('is_update', False) or item['is_update']
-                    merged['field_name'] = key
-
-                data.append(merged)
-
-        return DetailResponse(data=data)
+            # 遍历原始数据并填充结果字典
+            for item in data:
+                field_name = item.pop('field_name')
+                for key, value in item.items():
+                    if field_name in result:
+                        if value:
+                            result[field_name][key] = True
+                    else:
+                        result[field_name] = {}
+                        result[field_name][key] = value
+        return DetailResponse(data=result)
