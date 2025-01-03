b5c583ba7dcc8db7f81d9f1faedfd7ad582d23cc
李小涛 <1537080775@qq.com>
2025-01-03 14:41:30 +0800

    feat(20250103-role): 增加角色批量授权用户

--- web/src/views/system/role/components/RoleDrawer.vue
+++ web/src/views/system/role/components/RoleDrawer.vue
@@ -11,38 +11,56 @@
 		<template #header>
 			<div>
 				当前授权角色：
-				<el-tag>{{ RoleDrawer.roleName }}</el-tag>
+				<el-tag style="margin-right: 20px">{{ RoleDrawer.roleName }}</el-tag>
+				授权人员：
+				<el-button size="small" :icon="UserFilled" @click="handleUsers">{{ RoleDrawer.users.length }}</el-button>
 			</div>
 		</template>
 		<splitpanes class="default-theme" style="height: 100%">
 			<pane min-size="20" size="22">
 				<div class="pane-box">
-					<MenuTree />
+					<MenuTreeCom />
 				</div>
 			</pane>
 			<pane min-size="20">
 				<div class="pane-box">
 					<el-tabs v-model="activeName" class="demo-tabs">
-						<el-tab-pane label="接口权限" name="first"><MenuBtn /></el-tab-pane>
-						<el-tab-pane label="列字段权限" name="second"><MenuField /></el-tab-pane>
+						<el-tab-pane label="接口权限" name="first"><MenuBtnCom /></el-tab-pane>
+						<el-tab-pane label="列字段权限" name="second"><MenuFieldCom /></el-tab-pane>
 					</el-tabs>
 				</div>
 			</pane>
 		</splitpanes>
 	</el-drawer>
+
+	<el-dialog v-model="dialogVisible" title="授权用户" width="700px" :close-on-click-modal="false">
+		<RoleUsersCom />
+	</el-dialog>
 </template>
 
 <script setup lang="ts">
 import { Splitpanes, Pane } from 'splitpanes';
 import 'splitpanes/dist/splitpanes.css';
+import { UserFilled } from '@element-plus/icons-vue';
 import { RoleDrawerStores } from '../stores/RoleDrawerStores';
 import { defineAsyncComponent, ref } from 'vue';
+import { RoleUsersStores } from '../stores/RoleUsersStores';
 
-const MenuTree = defineAsyncComponent(() => import('./RoleMenuTree.vue'));
-const MenuBtn = defineAsyncComponent(() => import('./RoleMenuBtn.vue'));
-const MenuField = defineAsyncComponent(() => import('./RoleMenuField.vue'));
+const MenuTreeCom = defineAsyncComponent(() => import('./RoleMenuTree.vue'));
+const MenuBtnCom = defineAsyncComponent(() => import('./RoleMenuBtn.vue'));
+const MenuFieldCom = defineAsyncComponent(() => import('./RoleMenuField.vue'));
+const RoleUsersCom = defineAsyncComponent(() => import('./RoleUsers.vue'));
 const RoleDrawer = RoleDrawerStores(); // 抽屉参数
+const RoleUsers = RoleUsersStores(); // 角色-用户
 const activeName = ref('first');
+
+const dialogVisible = ref(false);
+
+const handleUsers = () => {
+	dialogVisible.value = true;
+	RoleUsers.get_all_users(); // 获取所有用户
+	RoleUsers.set_right_users(RoleDrawer.$state.users); // 设置已选中用户
+};
 </script>
 
 <style lang="scss" scoped>
--- web/src/views/system/role/index.vue
+++ web/src/views/system/role/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<fs-page>
 		<fs-crud ref="crudRef" v-bind="crudBinding"> </fs-crud>
-		<PermissionDrawerCom ref="PermissionDrawerCom" />
+		<PermissionDrawerCom />
 	</fs-page>
 </template>
 
@@ -12,20 +12,25 @@ import { createCrudOptions } from './crud';
 import { RoleDrawerStores } from './stores/RoleDrawerStores';
 import { RoleMenuBtnStores } from './stores/RoleMenuBtnStores';
 import { RoleMenuFieldStores } from './stores/RoleMenuFieldStores';
+import { RoleUsersStores } from './stores/RoleUsersStores';
 
 const PermissionDrawerCom = defineAsyncComponent(() => import('./components/RoleDrawer.vue'));
 
-const RoleDrawer = RoleDrawerStores(); // 权限配置抽屉参数
+const RoleDrawer = RoleDrawerStores(); // 角色-抽屉
 const RoleMenuBtn = RoleMenuBtnStores(); // 角色-菜单
-const RoleMenuField = RoleMenuFieldStores();
+const RoleMenuField = RoleMenuFieldStores();// 角色-菜单-字段
+const RoleUsers = RoleUsersStores();// 角色-用户
 const { crudBinding, crudRef, crudExpose } = useFs({
 	createCrudOptions,
-	context: { RoleDrawer, RoleMenuBtn,RoleMenuField },
+	context: { RoleDrawer, RoleMenuBtn, RoleMenuField },
 });
 
 // 页面打开后获取列表数据
 onMounted(async () => {
 	// 刷新
 	crudExpose.doRefresh();
+	// 获取全部用户
+	RoleUsers.get_all_users();
+
 });
 </script>
