49b8ba9afd692a6907e88503c3ed4611ddaf293c
allen_xiong <allen_xiong@avc.co>
2026-03-23 14:57:26 +0800

    日常优化
    1. 移除页面旧代码
    2. 杂采材质信息页面导入失败问题
    3. 其他优化

--- web/src/views/basicinfo/company/api.ts
+++
@@ -1,8 +0,0 @@
-import { request } from '/@/utils/service'
-
-const apiPrefix = '/api/system/companies/'
-
-export const GetList = (params: any) => request({ url: apiPrefix, method: 'get', params })
-export const AddObj = (data: any) => request({ url: apiPrefix, method: 'post', data })
-export const UpdateObj = (data: any) => request({ url: apiPrefix + data.id + '/', method: 'put', data })
-export const DelObj = (id: string | number) => request({ url: apiPrefix + id + '/', method: 'delete' })
