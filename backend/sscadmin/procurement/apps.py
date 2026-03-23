233720f73a0c9e5074804d838bf374e7522ce9ae
allen_xiong <allen_xiong@avc.co>
2026-03-23 16:58:36 +0800

    移除已弃用的原业务代码

--- backend/sscadmin/procurement/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProcurementConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "sscadmin.procurement"
-    verbose_name = "采购管理"
