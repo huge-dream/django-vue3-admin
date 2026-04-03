59fb4044603d3230e78b49397ba22bec15f26b69
nebula_chen <nebula_chen@avc.co>
2026-04-03 15:17:43 +0800

    feat(pisadmin)：新增供应商用户信息页

--- web/src/views/pisadmin/basicinfo/supplier_user/api.ts
+++ web/src/views/pisadmin/basicinfo/supplier_user/api.ts
@@ -3,3 +3,6 @@ import { request } from '/@/utils/service'
 const apiPrefix = '/api/pisadmin/basicinfo/supplier_users/'
 
 export const GetList = (params: any) => request({ url: apiPrefix, method: 'get', params })
+export const AddObj = (data: any) => request({ url: apiPrefix, method: 'post', data })
+export const UpdateObj = (data: any) => request({ url: apiPrefix + data.id + '/', method: 'put', data })
+export const DelObj = (id: string | number) => request({ url: apiPrefix + id + '/', method: 'delete' })
