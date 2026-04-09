e93eabb073bd2f3f417e57b8b83d2ec0a6c856ef
Lewis_yan <lewis_yan@avc.co>
2026-04-09 08:56:49 +0800

    fix(i18n): correct login i18n structure with proper message.pages.login nesting
    The login i18n files were exporting flat structures that didn't match
    the expected key paths like message.pages.login.loginTitle.
    Wrapped content in message.pages.login.* structure like other i18n modules.

--- web/src/i18n/pages/login/zh-cn.ts
+++ web/src/i18n/pages/login/zh-cn.ts
@@ -1,37 +1,43 @@
 // 定义内容
 export default {
-	label: {
-		one1: '账号密码登录',
-		two2: '手机号登录',
-		two3: '二维码登录',
-		changePwd: '密码修改',
-	},
-	link: {
-		one3: '第三方登录',
-		two4: '友情链接',
-	},
-	account: {
-		accountPlaceholder1: '请输入登录账号/邮箱/手机号',
-		accountPlaceholder2: '请输入登录密码',
-		accountPlaceholder3: '请输入验证码',
-		accountPlaceholder4:'请输入新密码',
-		accountPlaceholder5:'请再次输入新密码',
-		accountBtnText: '登 录',
-	},
-	mobile: {
-		placeholder1: '请输入手机号',
-		placeholder2: '请输入验证码',
-		codeText: '获取验证码',
-		btnText: '登 录',
-		msgText: '* 温馨提示：建议使用谷歌、Microsoft Edge，版本 79.0.1072.62 及以上浏览器，360浏览器请使用极速模式',
-	},
-	scan: {
-		text: '打开手机扫一扫，快速登录/注册',
-	},
-	signInText: '欢迎回来！',
-	loginTitle: '采购询报价系统 - 采购端',
-	supplierTitle: '采购询报价系统 - 供应商端',
-	firstLoginTip: '初次登录请修改密码',
-	welcomeLogin: '欢迎登录',
-	copyright: 'Copyright © AVC 版权所有',
+    message: {
+        pages: {
+            login: {
+                label: {
+                    one1: '账号密码登录',
+                    two2: '手机号登录',
+                    two3: '二维码登录',
+                    changePwd: '密码修改',
+                },
+                link: {
+                    one3: '第三方登录',
+                    two4: '友情链接',
+                },
+                account: {
+                    accountPlaceholder1: '请输入登录账号/邮箱/手机号',
+                    accountPlaceholder2: '请输入登录密码',
+                    accountPlaceholder3: '请输入验证码',
+                    accountPlaceholder4: '请输入新密码',
+                    accountPlaceholder5: '请再次输入新密码',
+                    accountBtnText: '登 录',
+                },
+                mobile: {
+                    placeholder1: '请输入手机号',
+                    placeholder2: '请输入验证码',
+                    codeText: '获取验证码',
+                    btnText: '登 录',
+                    msgText: '* 温馨提示：建议使用谷歌、Microsoft Edge，版本 79.0.1072.62 及以上浏览器，360浏览器请使用极速模式',
+                },
+                scan: {
+                    text: '打开手机扫一扫，快速登录/注册',
+                },
+                signInText: '欢迎回来！',
+                loginTitle: '采购询报价系统 - 采购端',
+                supplierTitle: '采购询报价系统 - 供应商端',
+                firstLoginTip: '初次登录请修改密码',
+                welcomeLogin: '欢迎登录',
+                copyright: 'Copyright © AVC 版权所有',
+            },
+        },
+    },
 };
